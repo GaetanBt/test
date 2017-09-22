9e0e83dbc958b0d379afcc9b3b00c2e324a378ab
Gaëtan <gaetan.bonnot@gmail.com>
2017-09-22 16:17:02 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
+const clean = require('gulp-clean')
+
+const htmlminConfig = {
+  collapseWhitespace: true,
+  minifyCSS: true
+}
+
+gulp.task('clean-dist', function () {
+  return gulp.src('dist', { read: false })
+    .pipe(clean())
+})
+
+gulp.task('minify', ['clean-dist'], function () {
+  return gulp.src('src/index.html')
+    .pipe(htmlmin(htmlminConfig))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('default', ['minify'])
